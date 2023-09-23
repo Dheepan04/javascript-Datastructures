ee22f28250c15a3465e53b0e8c1c939d771562b4
Dheepan04 <dheepangrasp@gmail.com>
2023-09-24 00:06:05 +0530

    closing the issues based on issue raised

--- linked-pop.js
+++ linked-pop.js
@@ -23,4 +23,23 @@ class singlyLL{
         this.length--
         return current
     }
+    print(){
+        var current=this.head
+        var arr=[]
+        while(current){
+            arr.push(current.val)
+           current= current.next
+        }
+
+        return arr
+    }
+
 }
+var list = new singlyLL()
+list.head=new node(1)
+list.head.next=new node(2)
+list.head.next.next=new node(3)
+list.head.next.next.next=new node(4)
+console.log(list.print())
+list.pop()
+console.log(list.print())
